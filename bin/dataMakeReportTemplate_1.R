f7599d9de0f25722b467489c0949925c1ab57f4f
Alberto Santos Delgado <alb.santosdel@gmail.com>
2024-01-17 09:17:09 +0100

    Refactored code -- python: functions / R: parameters, conditions

--- bin/dataMakeReportTemplate_1.R
+++ bin/dataMakeReportTemplate_1.R
@@ -1,58 +1,35 @@
-#!/usr/local/bin/Rscript
+#!/usr/bin/env Rscript
+# ______      _          _____      _                      ______ _       _    __                     
+# |  _  \    | |        /  ___|    (_)                     | ___ \ |     | |  / _|                    
+# | | | |__ _| |_ __ _  \ `--.  ___ _  ___ _ __   ___ ___  | |_/ / | __ _| |_| |_ ___  _ __ _ __ ___  
+# | | | / _` | __/ _` |  `--. \/ __| |/ _ \ '_ \ / __/ _ \ |  __/| |/ _` | __|  _/ _ \| '__| '_ ` _ \ 
+# | |/ / (_| | || (_| | /\__/ / (__| |  __/ | | | (_|  __/ | |   | | (_| | |_| || (_) | |  | | | | | |
+# |___/ \__,_|\__\__,_| \____/ \___|_|\___|_| |_|\___\___| \_|   |_|\__,_|\__|_| \___/|_|  |_| |_| |_|
+#
+#website: https://multiomics-analytics-group.github.io/
+#project: https://github.com/biosustain/dsp_nf-msdap
 
-## setup https://github.com/ftwkoopmans/msdap/blob/master/doc/userguide.md#spectronaut
-# we are testing the functionality of processing spectronaut data
+#@DTU Biosustain
 
-## This script is a development version and is only working with Spectronaut output at the moment
+arg <- commandArgs(TRUE)
+filenamepath <- arg[1] #  link the output file
+format <- arg[2]
 
-###############################################################################
-######### Set working directory and input file linking ########################
-###############################################################################
-
-#setwd('./') # Set working directory and execute to have Rstudio autocomplete
-
-###############################################################################
-arg<-commandArgs(TRUE)
-# set commandline param 1
-filenamepath = arg[1] #  link the output file
-
-
-###############################################################################
-## FASTA file or files: an array of filenames, these should be the full path ##
-###############################################################################
-
-
-# set command line param 2
-#fastafiles = arg[2]
-#c('','')
-###############################################################################
-
-print("print test 1")
-message("message test 1")
 library(msdap)
-print("print test2")
-message("message test 2")
 
-
-dataset <- import_dataset_spectronaut(
-  filename = filenamepath,
-  confidence_threshold = 0.01,
-  use_normalized_intensities = FALSE,
-  use_irt = TRUE,
-  return_decoys = FALSE,
-  remove_shared_spectronaut_proteingroups = FALSE,
-  do_plot = TRUE
-)
-
-
-# change to overwrite / append
-#dataset <- import_fasta(
-#  dataset,
-#  files = fastafiles
-#)
-
-# optional protein removal
-##### Placeholder for adding this step
+if (tolower(format) == "spectronaut") {
+  dataset <- import_dataset_spectronaut(
+    filename = filenamepath,
+    confidence_threshold = 0.01,
+    use_normalized_intensities = FALSE,
+    use_irt = TRUE,
+    return_decoys = FALSE,
+    remove_shared_spectronaut_proteingroups = FALSE,
+    do_plot = TRUE
+  )
+} else {
+  print(format)
+}
 
 # create a template file to describe your sample metadata
 write_template_for_sample_metadata(dataset, "samples.xlsx", overwrite = FALSE)
--- bin/get_conversion_table.py
+++ bin/get_conversion_table.py
@@ -1,80 +1,131 @@
 #!/usr/bin/env python3
+
+'''
+______      _          _____      _                      ______ _       _    __                     
+|  _  \    | |        /  ___|    (_)                     | ___ \ |     | |  / _|                    
+| | | |__ _| |_ __ _  \ `--.  ___ _  ___ _ __   ___ ___  | |_/ / | __ _| |_| |_ ___  _ __ _ __ ___  
+| | | / _` | __/ _` |  `--. \/ __| |/ _ \ '_ \ / __/ _ \ |  __/| |/ _` | __|  _/ _ \| '__| '_ ` _ \ 
+| |/ / (_| | || (_| | /\__/ / (__| |  __/ | | | (_|  __/ | |   | | (_| | |_| || (_) | |  | | | | | |
+|___/ \__,_|\__\__,_| \____/ \___|_|\___|_| |_|\___\___| \_|   |_|\__,_|\__|_| \___/|_|  |_| |_| |_|
+
+website: https://multiomics-analytics-group.github.io/
+project: https://github.com/biosustain/dsp_nf-msdap
+
+@DTU Biosustain
+'''
+
 import os
-import sys
+import re
 import urllib.request
-import hashlib
-import argparse 
+import argparse
+import pandas as pd
 
-# receive list of samples
-parser = argparse.ArgumentParser()
-# --linrary
-parser.add_argument("-l", "--library", help="parh to library folder for storing conversion file and proteome fasta files")
-
-args = parser.parse_args()
-
-
-# this file requires a conversion table from NCBI tax ID to uniprot
-# input is a mzTab file, as a loop, all species will be extracted, converted to uniprot syntax, and URLs will be generated.
-# a complete fasta proteome will be downloaded and saved locally.
-# the fasta will be filtered to also appear in a SwissProt only version.
-
-print(sys.version)
-
-# check if exists, and calculate md5
-library_folder = args.library
-dictionary_raw_file = ''.join([library_folder, '/reference_proteomes.txt'])
-conversion_table = ''.join([library_folder, '/conversion.tsv'])
-# if exists README // conversion table
-# md5 sum compare?
-
-# check exists conversion
-if not os.path.exists(library_folder):
-    os.mkdir(library_folder)
-
-if os.path.exists(dictionary_raw_file):
-    md5_returned = hashlib.md5(dictionary_raw_file).hexdigest()
-    print(md5_returned)
-else:
-    urllib.request.urlretrieve("https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/README", dictionary_raw_file)
+reference_proteomes_file_name = "reference_proteomes.txt"
+conversion_table_file_name = "conversion_table.tsv"
+#UniProt reference proteome table
+uniprot_reference_url = "https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/README"
+#UniProt download fasta
+uniprot_proteome_url = 'https://rest.uniprot.org/uniprotkb/stream?compressed=true&format=fasta&includeIsoform=true&query=(proteome:UID)'
 
 
-if os.path.exists(conversion_table):
-    md5_returned = hashlib.md5(conversion_table).hexdigest()
-    print(md5_returned)
-else:
-    # read file until header line that starts wiht Proteome_ID, export rest as tsv.
-    # extract Release date, etc.
-    dictionary_raw_md5 = 'void'
-    proteomelist_release = 'void'
-    proteomelist_speciescount = ''
-    tsvout = open(conversion_table, 'w', encoding="utf-8")
-    with open(dictionary_raw_file, encoding="utf-8") as f:
-        tsvparse = False
+def download_reference_proteomes(url: str, reference_proteomes_dir: str, force: bool=False):
+    '''
+    Downloads UniProt reference proteomes containing the table with the mapping between
+    UniProt proteome identifiers and NCBI taxonomic identifiers. When downloading, it 
+    also extracts the mapping table (conversion table) to a file.
+    
+    params:
+        str url: UniProt url with reference proteome file
+        str reference_proteomes_dir: Directory where the reference proteomes and \
+            conversion table can be found or should be stored
+        bool force: Boolean defining whether the file should be downloaded even if \
+            exists
+            
+    example:
+        python get_conversion_table.py --proteomes reference --force False
+    '''
+    reference_proteomes_file = os.path.join(reference_proteomes_dir, reference_proteomes_file_name)
+    if force or not os.path.exists(reference_proteomes_file):
+        if not os.path.exists(reference_proteomes_dir):
+            os.mkdir(reference_proteomes_dir)
+        urllib.request.urlretrieve(url, reference_proteomes_file)
+        
+        extract_conversion_table(reference_proteomes_dir)
+
+def extract_conversion_table(reference_proteomes_dir):
+    '''
+    Extracts the mapping table (conversion table) between UniProt proteome identifiers
+    and NCBI taxonomic identifiers from the reference proteome file and stores them into 
+    a file.
+    
+    params:
+        str reference_proteomes_dir: Directory where the reference proteomes and \
+            conversion table can be found or should be stored
+    '''
+    regex = r"=\s(\d+)"
+    reference_proteomes_file = os.path.join(reference_proteomes_dir, reference_proteomes_file_name)
+    conversion_table_file = os.path.join(reference_proteomes_dir, conversion_table_file_name)
+
+    with open(reference_proteomes_file, 'r', encoding="utf-8") as f:
+        n = 0
         for line in f:
-            if line[0:7] == 'Release' :
+            if line.startswith('Release'):
                 proteomelist_release = line
-            elif line[0:10] == 'Statistics':
-                proteomelist_speciescount += line
-            elif line[0:11] == 'Proteome_ID':
-                tsvparse = True
-            elif line[0:2] != 'UP':
-                tsvparse = False
-            if tsvparse == True:
-                tsvout.write(line)
-    tsvout.close()
-    print(proteomelist_release)
-    print(proteomelist_speciescount)
-    for file_name in [dictionary_raw_file,conversion_table]:
-        with open(file_name, 'rb') as file_to_check:
-            # read contents of the file
-            data = file_to_check.read()    
-            # pipe contents of the file through
-            md5_returned = hashlib.md5(data).hexdigest()
-            print(file_name, md5_returned)
+                print(proteomelist_release)
+            elif line.startswith("Statistics"):
+                match = re.search(regex, line)
+                if match:
+                    n = int(match.group(1))
+                    print(f"Proteomes stored:{n}")
+            elif line.startswith('Proteome_ID'):
+                header = line.rstrip().split("\t")
+                break
+        table_rows = range(0, n)
+        ref_prot = pd.read_csv(f, sep="\t", header=0, skiprows = lambda x: x not in table_rows)
+        ref_prot.columns = header
+        
+        with open(conversion_table_file, 'w', encoding="utf-8") as out:
+            ref_prot.to_csv(out, sep='\t', header=True, index=False, doublequote=None)
 
+def download_fasta(taxid: str, url: str, reference_proteomes_dir: str):
+    '''
+    Downloads from UniProt the fasta file for the taxid specified. This function
+    requires reading from the conversion table the UniProt identifier associated 
+    to the NCBI taxonomic identifier and downloading the file in compressed format.
+    
+    params:
+        str taxid: NCBI taxonomic identifier
+        str url: UniProt URL to download fasta file 
+        str reference_proteomes_dir: Directory where the reference proteomes and \
+            conversion table can be found or should be stored
+    '''
+    
+    conversion_table_file = os.path.join(reference_proteomes_dir, conversion_table_file_name)
+    conversion_df = pd.read_csv(conversion_table_file, sep="\t", dtype=str)
+    uniprot_id = conversion_df.loc[conversion_df['Tax_ID'] == taxid, 'Proteome_ID'].values[0]
+    url = url.replace('UID', str(uniprot_id))
+    fastaname = os.path.join(reference_proteomes_dir, f'{taxid}.fasta.gz')
+    urllib.request.urlretrieve(url, fastaname)
 
-# for line in dictionary_raw_file
-# if line starts with Release
-# save to variable along with statistics
-# then when line starts with Proteome_ID, save rest to tsv file
+if __name__ == "__main__":
+    # 3 parameters --taxid, --reference_proteomes and --force
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--proteomes", help="Path to file with the conversion between \
+        NCBI taxids and UniProt proteome identifiers")
+    parser.add_argument("-f", "--force", help="Whether or not force download of the reference \
+        proteomes", default=False)
+    parser.add_argument("-t", "--taxid", help="NCBI taxonomic identifier of interest", default=None)
+    args = parser.parse_args()
 
+    reference_proteomes = args.proteomes
+    force_dwn = args.force
+    taxid = args.taxid
+
+    download_reference_proteomes(url=uniprot_reference_url,
+                                 reference_proteomes_dir=reference_proteomes,
+                                 force=force_dwn)
+    if taxid is not None:
+        output_file_path = os.path.join(reference_proteomes, f'proteome_{taxid}.fasta.gz')
+        if not os.path.exists(output_file_path):
+            download_fasta(taxid=taxid, url=uniprot_proteome_url,
+                        reference_proteomes_dir=reference_proteomes)
